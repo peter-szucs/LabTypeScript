e517343a9ce4f5fd25b1eeb171458b02a6efe841
Peter Szücs <pepo_szucs@hotmail.com>
2021-01-19 14:33:31 +0100

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,12 +1,106 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, FlatList, Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface shoppingItem {
+  id: number,
+  title: string,
+  price: number,
+  description?: string,
+  image: string
+}
+
+type listItemProps = {
+  item: shoppingItem,
+  index: number,
+  //setIndex: number,
+
+}
+
+const CatalogList = (props:{ data: shoppingItem[] }) => {
+  
+  const [selected, setSelected] = useState(-1)
+  
+  return ( 
+    <SafeAreaView style={{ width: '100%', justifyContent: 'space-evenly' }}>
+      <View>
+      <FlatList
+        style={{backgroundColor: '#f2f2f2', padding: 10}}
+        data={props.data}
+        renderItem={({ item, index }) => 
+          <ItemComponent 
+            item={item} 
+            index={index} 
+            //setIndex={setSelected} 
+            /> }
+          ItemSeparatorComponent={() => {
+            return (
+              <View
+                style= {{ width: '100%', height: 1, backgroundColor: '#e2e2e2'}}>
+              </View>
+            )
+          }}
+        keyExtractor={(item, index) => index.toString()} />
+    </View>
+    </SafeAreaView>
+    
+  );
+}
+
+async function fetchCatalog(): Promise<shoppingItem[]> {
+  const url =  'https://fakestoreapi.com/products'
+  let catalog: shoppingItem[] = []
+  let response = await fetch(url)
+  if(response.status == 200) {
+    let body: shoppingItem[] = await response.json()
+    body.forEach((item: shoppingItem) => {
+      let newItem: shoppingItem = {
+        id: item.id,
+        title: item.title,
+        price: item.price,
+        image: item.image
+      }
+      catalog.push(newItem)
+    })
+    return catalog
+  } else {
+    return catalog
+  }
+
+}
+
+const ItemComponent = ({ item, index }: listItemProps) => {
+  //const [selected, setSelected] = useState(-1)
+  return (
+    <TouchableOpacity onPress={() => console.log(item.id)}>
+      <View style={{ padding: 10, width: '80%' }}>
+        <View style={{ 
+          padding: 20, 
+          borderRadius: 10,  
+          flexDirection: 'row',
+          }}
+          >
+          <Image 
+            style={{ height: 80, width: 80 }}
+            source = {{ uri: item.image }} />
+          <Text style={{ fontWeight: 'bold', padding: 10 }}>{item.title}</Text>
+        </View>
+    </View>
+    </TouchableOpacity>
+  );
+}
 
 export default function App() {
+  const [data, setData] = useState<shoppingItem[]>([])
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CatalogList data={data}/>
+      <Button 
+        title="fetch" 
+        onPress={ async () => {
+          let data: shoppingItem[] = await fetchCatalog()
+          setData(data)
+        }} />
     </View>
   );
 }
@@ -15,7 +109,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 });
